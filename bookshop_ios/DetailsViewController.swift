1d592f354dd64c86221e03afee73ca991f31a631
Alexey Smirnov <alexey@gmail.com>
2016-04-07 16:57:50 +0800

    improved BookDetails view

--- bookshop_ios/DetailsViewController.swift
+++ bookshop_ios/DetailsViewController.swift
@@ -12,21 +12,41 @@ class DetailsViewController : UITableViewController {
 
     @IBOutlet weak var icon: UIImageView!
     @IBOutlet weak var book_title: RWLabel!
+    @IBOutlet weak var book_descr: RWLabel!
     
     var bookIndex : String!
+
+    let detailsSchema = [("author", "Author"),
+                         ("translator", "Translator"),
+                         ("language", "Language"),
+                         ("pages", "Number of pages"),
+                         ("publisher", "Publisher"),
+                         ("date_created", "Date added")]
     
+    var details = [String:String!]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let ref = Firebase(url: "\(Firebase_url)/details/\(bookIndex)")
         
-        ref.observeSingleEventOfType(.Value, withBlock: { snapshot  in
-
-            let item = BookData(snapshot: snapshot)
+        ref.observeEventType(.Value, withBlock: { snapshot  in
+            let title = snapshot.value["title"] as! String
+            let image = snapshot.value["image"] as! String
+            let description = snapshot.value["description"] as! String
+            
+            
+            for (code,_) in self.detailsSchema {
+                self.details[code] = snapshot.value[code] as! String
+            }
+            
             let cell = self.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0))
                 
-            self.icon.downloadedFrom(link: item.image, contentMode: .ScaleAspectFit, cell: cell!)
-            self.book_title.text = item.title
+            self.icon.downloadedFrom(link: image, contentMode: .ScaleAspectFit, cell: cell!)
+            self.book_title.text = title
+            self.book_descr.text = description
+            
+            self.tableView.reloadData()
         })
     }
     
@@ -39,9 +59,53 @@ class DetailsViewController : UITableViewController {
         return size.height+1.0
     }
     
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 1 && details.count == 0 {
+            return 0
+        } else {
+            return super.tableView(tableView, numberOfRowsInSection: section)
+        }
+    }
+    
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if (indexPath.section == 1 && details[detailsSchema[indexPath.row].0]!.characters.count == 0) {
+            return 0
+        }
+
         let cell : UITableViewCell = self.tableView(tableView, cellForRowAtIndexPath: indexPath)
-        return calculateHeightForCell(cell)
+        let height = calculateHeightForCell(cell)
+
+        return height
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAtIndexPath: indexPath)
+
+        if indexPath.section != 1 || details.count == 0 {
+            return cell
+        }
+
+        cell.textLabel!.text = Translate.s(detailsSchema[indexPath.row].1)
+        let value = details[detailsSchema[indexPath.row].0]
+        
+        if (detailsSchema[indexPath.row].0 == "date_created") {
+            
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+            
+            let date = dateFormatter.dateFromString(value!)
+        
+            let formatter = NSDateFormatter()
+            formatter.dateStyle = .ShortStyle
+            formatter.timeStyle = .NoStyle
+            
+            cell.detailTextLabel!.text = formatter.stringFromDate(date!)
+            
+        } else {
+            cell.detailTextLabel!.text = value
+        }
+        
+        return cell
     }
 
     @IBAction func done(sender: AnyObject) {
--- convert2firebase.py
+++ convert2firebase.py
@@ -12,7 +12,6 @@ for image in r.json():
     if not product in images:
         images[product] = image["fields"]["original"]
 
-
 r = requests.get('http://www.orthodoxbookshop.asia/static/product.json')
 output = {"index": {},
           "details": {}}
@@ -26,21 +25,22 @@ for book in  r.json():
                                         "image"         : "http://orthodoxbookshop.asia/media/" + images[book["pk"]]
         }
 
-#with io.open('books.json', 'w', encoding='utf-8') as f:
-#  f.write(unicode(json.dumps(output, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)))
-
-#output = {"details": {}}
-
 for book in  r.json():
     download_url = book["fields"]["download_url"]
     if download_url and "paidbooks" not in download_url:
 
-        t = lxml.html.fromstring(book["fields"]["description_en"])
+        description = book["fields"]["description_en"].replace('\r\n', '')
+        t = lxml.html.fromstring(description)
 
         output["details"][book["pk"]] = { "title"       : book["fields"]["title_en"],
                                         "description"   : t.text_content(),
                                         "date_created"  : book["fields"]["date_created"],
-                                        "image"         : "http://orthodoxbookshop.asia/media/" + images[book["pk"]]
+                                        "image"         : "http://orthodoxbookshop.asia/media/" + images[book["pk"]],
+                                        "author"        : book["fields"]["author_en"],
+                                        "translator"    : book["fields"]["translator"],
+                                        "language"      : book["fields"]["text_script"],
+                                        "pages"         : str(book["fields"]["num_pages"]),
+                                        "publisher"     : book["fields"]["publisher"],
         }
 
 with io.open('books.json', 'w', encoding='utf-8') as f:
